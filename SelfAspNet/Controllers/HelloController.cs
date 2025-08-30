cbb5e4e5c5a7a52fecfd2f746e3032fbf96540aa
konoshin <shinyakono@outlook.jp>
2025-08-31 00:25:57 +0900

    HelloWorldを表示（Controllerのみ使用）

---
+++ SelfAspNet/Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SelfAspNet.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello World!");
+        }
+    }
+}
