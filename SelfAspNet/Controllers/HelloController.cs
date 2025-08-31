09798188858c7971c705a45ad7ef0607a78365e8
konoshin <shinyakono@outlook.jp>
2025-08-31 19:50:29 +0900

    Bookテーブルの内容を表示するページを追加

--- SelfAspNet/Controllers/HelloController.cs
+++ SelfAspNet/Controllers/HelloController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SelfAspNet.Models;
 
 namespace SelfAspNet.Controllers
 {
     public class HelloController : Controller
     {
+        private readonly MyContext _db;
+
+        public HelloController(MyContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return Content("Hello World!");
+        }
+
+        public IActionResult List()
+        {
+            var books = _db.Books;
+            return View(books);
         }
 
         public IActionResult Show()
